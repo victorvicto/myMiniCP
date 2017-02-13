d1311036897c905333032fbb2d5dd32d8dc4ca68
pschaus <pschaus@gmail.com>
2017-02-13 04:49:52 +0100

    cleaning

--- src/main/java/minicp/util/InconsistencyException.java
+++ src/main/java/minicp/util/InconsistencyException.java
@@ -15,9 +15,7 @@
 
 package minicp.util;
 
-/**
- * Created by ldm on 1/9/17.
- */
+
 public class InconsistencyException extends Throwable {
     private static final long serialVersionUID = 1240061199250453776L;
     public String toString() { return "inconsistency";}
