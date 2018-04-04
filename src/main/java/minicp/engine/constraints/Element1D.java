2b6e617fb398890e699aa4cd1e6f766eb82bd7d0
Victor Demu <victor.demu@gmail.com>
2018-04-04 15:20:46 +0200

    added the code to make Element1DTest succeed

--- src/main/java/minicp/engine/constraints/Element1D.java
+++ src/main/java/minicp/engine/constraints/Element1D.java
@@ -29,6 +29,28 @@ import java.util.Collections;
 
 public class Element1D extends Constraint {
 
+    private final int[] T;
+    private final IntVar y, z;
+    private int n;
+
+    private final ReversibleInt low;
+    private final ReversibleInt up;
+    private final ArrayList<Element1D.Tripple> yz;
+
+    private class Tripple implements Comparable<Element1D.Tripple> {
+        protected final int y,z;
+
+        private Tripple(int y, int z) {
+            this.y = y;
+            this.z = z;
+        }
+
+        @Override
+        public int compareTo(Element1D.Tripple t) {
+            return z - t.z;
+        }
+    }
+
     /**
      * T[y] = z
      * @param T
@@ -37,14 +59,51 @@ public class Element1D extends Constraint {
      */
     public Element1D(int[] T, IntVar y, IntVar z) {
         super(y.getSolver());
-        // TODO if Element1D extends Element2D
-        throw new NotImplementedException("Element1D");
+
+        this.T = T;
+        this.y = y;
+        this.z = z;
+        n = T.length;
+
+        this.yz = new ArrayList<Element1D.Tripple>();
+        for (int i = 0; i < T.length; i++) {
+            yz.add(new Element1D.Tripple(i,T[i]));
+        }
+        Collections.sort(yz);
+        low = new ReversibleInt(cp.getTrail(),0);
+        up = new ReversibleInt(cp.getTrail(),yz.size()-1);
     }
 
     @Override
     public void post() throws InconsistencyException {
-        // TODO (if not extending Element2D)
-        throw new NotImplementedException("Element1D");
+        y.removeBelow(0);
+        y.removeAbove(n-1);
 
+        y.propagateOnDomainChange(this);
+        z.propagateOnBoundChange(this);
+        propagate();
+    }
+
+    @Override
+    public void propagate() throws InconsistencyException {
+        int l = low.getValue();
+        int u = up.getValue();
+        int zMin = z.getMin();
+        int zMax = z.getMax();
+
+        while (yz.get(l).z < zMin || !y.contains(yz.get(l).y)) {
+            y.remove(yz.get(l).y);
+            l++;
+            if (l > u) throw new InconsistencyException();
+        }
+        while (yz.get(u).z > zMax || !y.contains(yz.get(u).y)) {
+            y.remove(yz.get(u).y);
+            u--;
+            if (l > u) throw new InconsistencyException();
+        }
+        z.removeBelow(yz.get(l).z);
+        z.removeAbove(yz.get(u).z);
+        low.setValue(l);
+        up.setValue(u);
     }
 }
